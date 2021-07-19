9aecd60d4a78678f27c98056c78ae7f28d70a2df
vgherard <vgherard@sissa.it>
2021-07-19 11:55:11 +0200

    Heuristic LaTeX preprocessing (close #1)

--- R/preprocess.R
+++ R/preprocess.R
@@ -1,9 +1,18 @@
+library(reticulate, warn.conflicts = F, quietly = T)
+library(dplyr)
+latex2text <- reticulate::import("pylatexenc.latex2text")
+decode_latex <- function(x) {
+	try(x <- latex2text$latex2text(x, tolerant_parsing = T), silent = T)
+	x
+	}
+
 text_preprocess <- function(x) {
 	x %>%
+		vapply(decode_latex, "", USE.NAMES = F) %>%
 		gsub("\\n", " ", .) %>%
 		kgrams::preprocess(
-			# Remove characters not matched by the regex
-			erase = "[^-.?!:;()'[:alnum:][:space:]]",
+			# Remove characters matched by the regex
+			erase = "",
 			# Put everything to lower case
 			lower_case = TRUE
 		)
