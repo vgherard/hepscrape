5787c823409284b8372d4e1e83f9970444bc16ad
vgherard <vgherard@sissa.it>
2021-07-19 09:05:55 +0200

    Use ln(idf) ^ 1.5 for word cloud

--- R/word_cloud.R
+++ R/word_cloud.R
@@ -19,43 +19,44 @@ vocab <- create_vocabulary(it)
 vectorizer <- vocab_vectorizer(vocab)
 dtm <- create_dtm(it, vectorizer)
 dtm_binary <- dtm > 0
-tfidf <- TfIdf$new(smooth_idf = F, norm = "l1")
-dtm_tfidf <- fit_transform(dtm, tfidf)
-binary_tfidf <- TfIdf$new(smooth_idf = F, norm = "none")
-dtm_binary_tfidf <- fit_transform(dtm_binary, binary_tfidf)
 
-tfidf_avg <- Matrix::colMeans(dtm_tfidf[1:100, ])
-tf_avg <- Matrix::colMeans(dtm[1:100, ])
-binary_tfidf_avg <- Matrix::colMeans(dtm_binary_tfidf[1:100, ])
-binary_tf_avg <- Matrix::colMeans(dtm_binary[1:100, ])
-words <- dtm_tfidf@Dimnames[[2]]
+lnorm <- Matrix::Diagonal(x = 1 / Matrix::rowSums(dtm))
+tf <- lnorm %*% dtm
+tf_avg_at_n <- Matrix::colMeans(tf[1:100, ])
+btf_avg_at_n <- Matrix::colMeans(dtm_binary[1:100, ])
 
+idf <- log(1 / Matrix::colMeans(dtm_binary))
+
+
+size <- tf_avg_at_n * idf ^ 1.5
+color <- btf_avg_at_n * idf ^ 1.5
+
+
+words <- names(size)
 set.seed(840)
 tbl <- tibble(
 	word = words,
-	size = tfidf_avg,
-	color = binary_tfidf_avg
+	size = size,
+	color = color
 	) %>%
-	filter(!(word %in% stopwords::stopwords(source = "smart"))) %>%
 	arrange(desc(size)) %>%
 	head(100) %>%
 	mutate(rand_angle = -45 + 11.25 * sample(0:8, n(), replace = T)) %>%
-	#mutate(rand_angle = 90 * sample(0:1, n(), replace = T)) %>%
 	{.}
 
 plot <- ggplot(tbl,
        aes(label = word, size = size
-           ,angle = rand_angle
            ,color = color
+           ,angle = rand_angle
        )
 ) +
 	geom_text_wordcloud(
 		shape = "circle", eccentricity = 1, show.legend = T) +
 	theme_minimal() +
 	scale_color_gradient(low = "blue", high = "red", guide = "colourbar") +
-	labs(color = "binary tf-idf  (avg @ last 100)",
-	     size = "tf-idf (avg @ last 100)",
-	     title = "Trends in hep-ph",
+	labs(size = "tf-idf (avg @ last 100)",
+	     color = "tp-idf  (avg @ last 100)",
+	     title = "Trending words in hep-ph abstracts",
 	     subtitle = paste("Last update:", Sys.Date())
 	     )
 
